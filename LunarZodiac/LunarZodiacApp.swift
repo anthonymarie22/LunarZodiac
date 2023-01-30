2a285fdd8db24319edf8382b2aea9400c8c853f0
anthonymarie22 <72616953+anthonymarie22@users.noreply.github.com>
2023-01-30 18:02:06 -0500

    Initial Commit

---
+++ LunarZodiac/LunarZodiacApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunarZodiacApp.swift
+//  LunarZodiac
+//
+//  Created by Anthony Marie on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct LunarZodiacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
