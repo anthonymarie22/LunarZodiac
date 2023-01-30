2b15154b2892fda184a6f787911050f226070eb4
anthonymarie22 <72616953+anthonymarie22@users.noreply.github.com>
2023-01-30 18:28:54 -0500

    Lunar with loops

--- LunarZodiac/ContentView.swift
+++ LunarZodiac/ContentView.swift
@@ -8,19 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var year = 2023
+    @State private var imageNumber = 3
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            Text(String(year))
+                .fontWeight(.black)
+                .foregroundColor(.orange)
+                .font(.largeTitle)
+            
+            
+            Image("image\(imageNumber)")
+                .resizable()
+                .scaledToFit()
+            
+            
+            
+            HStack{
+                
+                
+                Button("<") {
+                    year -= 1
+                    imageNumber -= 1
+                    if imageNumber < 0 {
+                        imageNumber = 11
+                    }
+                }
+                Spacer()
+                Button(">") {
+                    year += 1
+                    imageNumber += 1
+                    if imageNumber > 11 {
+                        imageNumber = 0
+                        
+                    }
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.orange)
+            .padding()
+            .font(.largeTitle)
+            .fontWeight(.black)
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
